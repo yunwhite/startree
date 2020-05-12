a8ab210ef5d43808346e2db25aa71a63b72b2ccf
yunwhite <s2018s12@e-mirim.hs.kr>
2020-05-12 22:43:35 +0900

    별 나무

--- startree/Program.cs
+++ startree/Program.cs
@@ -10,7 +10,14 @@ namespace startree
     {
         static void Main(string[] args)
         {
-
+            for (int i = 1; i <= 8; i++)
+            {
+                for (int x = 8 - i; x > 0; x--)
+                    Console.Write(" ");
+                for (int y = 1; y <= i * 2 - 1; y++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
         }
     }
 }
